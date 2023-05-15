01c9b1bd24d5f68f177127744e3d0e28277b6ae7
justin chau <jchau623@gmail.com>
2023-05-15 15:07:17 -0400

    song details page

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,24 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { DetailsHeader, Error, Laoder, RelatedSongs } from '../components';
+
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
+
+const SongDetails = () => {
+  const dispatch = useDispatch();
+  const { songId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player)
+
+  return (
+    <div className="flex flex-col">
+      {/* <DetailsHeader artistId={artistId} songData={songData}/> */}
+
+      <div className="mb-10">
+        <h2 className="text-white text-3x1 font-bold">Lyrics:</h2>
+        <div className="mt-5" />
+      </div>
+    </div>
+  );
+};
 
 export default SongDetails;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -12,6 +12,7 @@ export const shazamCoreApi = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/track' }),
+    getSongDetails: builder.query({ query: ({ songId }) => '/charts/track' }),
   }),
 });
 
