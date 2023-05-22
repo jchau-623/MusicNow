dc07244af622039d98421af1a27b01261409d04f
justin chau <jchau623@gmail.com>
2023-05-22 13:08:59 -0400

    test

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -56,6 +56,7 @@ const TopPlay = () => {
     dispatch(playPause(true));
   };
 
+  
   return (
     <div ref={divRef} className="x;:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
       <div className="w-full flex flex-col">
