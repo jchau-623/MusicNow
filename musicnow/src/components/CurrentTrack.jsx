18004f8191bb38d5ffd5dc82a7d4ee4c9571a9da
justin chau <jchau623@gmail.com>
2023-07-31 21:27:11 -0400

    test

--- musicnow/src/components/CurrentTrack.jsx
+++ musicnow/src/components/CurrentTrack.jsx
@@ -16,6 +16,7 @@ export default function CurrentTrack() {
           },
         }
       );
+      
       if (response.data !== "") {
         const currentPlaying = {
           id: response.data.item.id,
